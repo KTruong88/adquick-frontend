1b206eacb28e0c060386264af9d36137e6470b47
Kevin Truong <kevintru0ng@yahoo.com>
2018-05-03 12:06:05 -0700

    navbar complete

---
+++ src/components/Api.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+class ApiCall extends React.Component {
+   constructor(props) {
+    super(props);
+    this.state = {
+      value: []
+    }
+  }
+
+  callApi() {
+    const { value } = this.state
+
+    fetch('http://localhost:3001/billboards')
+    .then((result) => {
+      return result.json()
+    })
+    .then((jsonResult) => {
+      this.setState({
+        value: jsonResult
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  }
+
+  render() {
+    const { value } = this.state
+    return (
+      <div>
+        <button onClick={() => this.callApi()}>
+          Click here to call API
+        </button>
+        <div>
+          Title: { value.title } <br />
+          UserId: { value.userId } <br />
+          Id: { value.id } <br />
+          Body: { value.body }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ApiCall
